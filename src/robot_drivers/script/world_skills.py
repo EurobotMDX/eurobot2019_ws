418571e2497fd245982a52dc373f94c8744af5c3
robot_odroid <co607@live.mdx.ac.uk>
2016-02-11 19:22:09 +0000

    world skills

--- src/robot_drivers/script/world_skills.py
+++ src/robot_drivers/script/world_skills.py
@@ -9,6 +9,8 @@ from robot_interface_advanced import AdvancedRobotInterface
 # Simon Klimek 22 May WorldSkills task
 # draw square and triangle with given length
 
+rospy.init_node("eurobot_task_handler", anonymous=False)
+
 rospy.loginfo("initializing the robot...")
 
 robot = AdvancedRobotInterface()
@@ -21,13 +23,20 @@ robot.wait_for_pull_to_start(state=True); rospy.sleep(3.0)
 
 # driving
 def drive_square(dist):
-    robot.move_linear(dist)
+    robot.move_linear(dist, should_avoid_obstacles=True, obstacle_backup_distance=0.1, clearing_distance=0.12)
     robot.move_angular(90)
-    robot.move_linear(dist)
+    robot.update_robot_score(50)
+    robot.move_linear(dist, should_avoid_obstacles=True, obstacle_backup_distance=0.1, clearing_distance=0.12)
     robot.move_angular(90)
-    robot.move_linear(dist)
+    robot.update_robot_score(50)
+    robot.move_linear(dist, should_avoid_obstacles=True, obstacle_backup_distance=0.1, clearing_distance=0.12)
     robot.move_angular(90)
+    robot.update_robot_score(50)
+    robot.move_linear(dist, should_avoid_obstacles=True, obstacle_backup_distance=0.1, clearing_distance=0.12)
+    robot.move_angular(90)
+    robot.update_robot_score(50)
 
+robot.deactivate_experiment()
 drive_square(0.5)
 
 def drive_triangle():

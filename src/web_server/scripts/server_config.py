628ebecf19add28f6ea1426758b60002c9baa5ea
Kingusha23 <kingusha2333@gmail.com>
2018-12-14 19:56:18 +0000

    web server test

--- src/web_server/scripts/server_config.py
+++ src/web_server/scripts/server_config.py
@@ -1,4 +1,4 @@
-import thread
+import threading
 import os, sys
 from flask import Flask, render_template, request, session, redirect, escape, render_template_string, Response, send_from_directory, url_for
 

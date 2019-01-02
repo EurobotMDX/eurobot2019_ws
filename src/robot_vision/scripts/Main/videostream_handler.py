8e91950c1005fae88eff0766d3b475a29b8ea875
simonklimek <simonklimek91@gmail.com>
2019-01-02 22:20:58 +0000

    puck-position [x, y] plus ROI

--- src/robot_vision/scripts/Main/videostream_handler.py
+++ src/robot_vision/scripts/Main/videostream_handler.py
@@ -11,42 +11,58 @@ import time
 import cv2
 import numpy as np
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--picamera", type=int, default=-1,
-	help="whether or not the Raspberry Pi camera should be used")
-args = vars(ap.parse_args())
+# # construct the argument parse and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-p", "--picamera", type=int, default=-1,
+# 	help="whether or not the Raspberry Pi camera should be used")
+# args = vars(ap.parse_args())
 
-def nothing(x):
-    #any operation with change trackbar
-    pass
+# def nothing(x):
+#     #any operation with change trackbar
+#     pass
 
-cv2.namedWindow("Trackbars")
+# cv2.namedWindow("Trackbars")
 
-cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
-cv2.createTrackbar("L - S", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("L - V", "Trackbars", 0, 255, nothing)
-cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
-cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
-cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
+# cv2.createTrackbar("L - H", "Trackbars", 0, 179, nothing)
+# cv2.createTrackbar("L - S", "Trackbars", 130, 255, nothing)
+# cv2.createTrackbar("L - V", "Trackbars", 255, 255, nothing)
+# cv2.createTrackbar("U - H", "Trackbars", 179, 179, nothing)
+# cv2.createTrackbar("U - S", "Trackbars", 255, 255, nothing)
+# cv2.createTrackbar("U - V", "Trackbars", 255, 255, nothing)
 
-font = cv2.FONT_HERSHEY_COMPLEX
+# font = cv2.FONT_HERSHEY_COMPLEX
 
 
-# initialize the video stream and allow the cammera sensor to warmup
-vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
-time.sleep(2.0)
+# # initialize the video stream and allow the cammera sensor to warmup
+# vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
+# time.sleep(2.0)
+
+RESCALE_FACTOR = 0.5
+vs = cv2.VideoCapture(0)
+
+vs.set(3, int(1280*RESCALE_FACTOR))
+vs.set(4, int(720*RESCALE_FACTOR))
+
+#
+for i in range(15):
+    _, frame = vs.read()
+
+r = cv2.selectROI(frame)
 
 # loop over the frames from the video stream
 while True:
+	_, frame = vs.read()
 	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 400 pixels
-	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
-	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+	# # to have a maximum width of 400 pixels
+	# frame = vs.read()
+	# frame = imutils.resize(frame, width=400)
+	img_crop = frame[int(r[1]):int(r[1]+r[3]), int(r[0]):int(r[0]+r[2])]
+	hsv = cv2.cvtColor(img_crop, cv2.COLOR_BGR2HSV)
+
+	
 
     # definig the range of red color
-	red_lower = np.array([0, 130, 255])
+	red_lower = np.array([0, 130, 250])
 	red_upper = np.array([180, 255, 255])
 
     # defining the Range of Blue color
@@ -66,30 +82,48 @@ while True:
     # kernal = np.ones((5, 5), "uint8")
 
     # red_mask = cv2.dilate(red_mask, kernal)
-	res = cv2.bitwise_and(frame, frame, mask=red_mask)
+	res = cv2.bitwise_and(img_crop, img_crop, mask=red_mask)
 
     # blue_mask = cv2.dilate(blue_mask, kernal)
-	res1 = cv2.bitwise_and(frame, frame, mask=blue_mask)
+	#res1 = cv2.bitwise_and(frame, frame, mask=blue_mask)
 
     # green_mask = cv2.dilate(green_mask, kernal)
-	res2 = cv2.bitwise_and(frame, frame, mask=green_mask)
+	#res2 = cv2.bitwise_and(frame, frame, mask=green_mask)
 
 	
-	kernel = np.ones((15,15),np.float32)/225
-	smoothed = cv2.filter2D(res,-1,kernel)
-	#smoothed = cv2.filter2D(res1,-1,kernel)
-	#smoothed = cv2.filter2D(res2,-1,kernel)
-	cv2.imshow('Original',frame)
-	cv2.imshow('Averaging',smoothed)
+	# kernel = np.ones((15,15),np.float32)/225
+	# smoothed = cv2.filter2D(res,-1,kernel)
+	# #smoothed = cv2.filter2D(res1,-1,kernel)
+	# #smoothed = cv2.filter2D(res2,-1,kernel)
+	# cv2.imshow('Original',frame)
+	# cv2.imshow('Averaging',smoothed)
 
-	_, puck = cv2.threshold(smoothed, 30, 255, cv2.THRESH_BINARY)
+
+
+
+	blur = cv2.GaussianBlur(res, (15, 15), 0)
+	cv2.imshow('Gaussian Blurring', blur)
+
+
+	_, puck = cv2.threshold(blur, 70, 255, cv2.THRESH_BINARY)
 	cv2.imshow('Puck',puck)
 
-	print(puck)
-    # x, y
-    # y, x
-	print(np.where(puck!=0)[0])
-	print(np.where(puck!=0)[1])
+	xs = np.where(puck != 0)[1]
+	ys = np.where(puck != 0)[0]
+
+	xstd = np.std(xs)
+	ystd = np.std(ys)
+
+	x_init_avg = np.mean(xs)
+	y_init_avg = np.mean(ys)
+
+	xs = [x for x in xs if x <= x_init_avg+xstd or x >= x_init_avg-xstd]
+	ys = [y for y in ys if y <= y_init_avg+xstd or y >= y_init_avg-xstd]
+	xavg = np.mean(xs)
+	yavg = np.mean(ys)
+
+	print(xavg, yavg)
+
 
 	key = cv2.waitKey(1) & 0xFF
 

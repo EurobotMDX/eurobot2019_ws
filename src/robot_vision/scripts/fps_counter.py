9b2dcec3166b7f84af52d08a4cba728f263b9d51
simonklimek <simonklimek91@gmail.com>
2019-01-04 09:45:29 +0000

    fps counter fix

--- src/robot_vision/scripts/fps_counter.py
+++ src/robot_vision/scripts/fps_counter.py
@@ -16,23 +16,23 @@ if __name__ == '__main__' :
      
     if int(major_ver)  < 3 :
         fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
-        print "Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps)
+        print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
     else :
         fps = video.get(cv2.CAP_PROP_FPS)
-        print "Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps)
+        print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
      
  
     # Number of frames to capture
     num_frames = 120;
      
      
-    print "Capturing {0} frames".format(num_frames)
+    print("Capturing {0} frames".format(num_frames))
  
     # Start time
     start = time.time()
      
     # Grab a few frames
-    for i in xrange(0, num_frames) :
+    for i in ssssssrange(0, num_frames) :
         ret, frame = video.read()
  
      
@@ -41,11 +41,11 @@ if __name__ == '__main__' :
  
     # Time elapsed
     seconds = end - start
-    print "Time taken : {0} seconds".format(seconds)
+    print("Time taken : {0} seconds".format(seconds))
  
     # Calculate frames per second
     fps  = num_frames / seconds;
-    print "Estimated frames per second : {0}".format(fps);
+    print("Estimated frames per second : {0}".format(fps))
  
     # Release video
     video.release()

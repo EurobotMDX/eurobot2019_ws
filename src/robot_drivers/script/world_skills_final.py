0b41302d3255f74a75f100359143132c96f8c560
robot_odroid <co607@live.mdx.ac.uk>
2019-11-02 18:59:16 +0000

    added sonar

--- src/robot_drivers/script/world_skills_final.py
+++ src/robot_drivers/script/world_skills_final.py
@@ -14,11 +14,30 @@ robot.initialize()
 rospy.loginfo("waiting for pull to start")
 robot.wait_for_pull_to_start(state=True)
 
+
+rospy.loginfo("Testing Grippers")
+robot.open_gripper()
+time.sleep(1)
+robot.close_gripper()
+time.sleep(1)
+
+
+rospy.loginfo("Testing Half Grippers")
+robot.semi_open_gripper()
+time.sleep(1)
+robot.semi_close_gripper()
+time.sleep(1)
+
+
+rospy.loginfo("Testing Angular Velocity")
+robot.move_angular(180)
+robot.move_angular(180)
+robot.move_angular(180)
+robot.move_angular(180)
+
+
+rospy.loginfo("Testing Linear Velocity")
 robot.move_linear(0.1, should_avoid_obstacles=True, obstacle_backup_distance=0.2, clearing_distance=0.32)
-robot.move_angular(180)
-robot.move_angular(180)
-robot.move_angular(180)
-robot.move_angular(180)
 
 
 rospy.loginfo("ctrl-c to terminate")
